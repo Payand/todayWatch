29de8937099b2a26926ffba054b30428b7ff65d2
Ali shooshtari <shooshtariali05@gmail.com>
2022-09-18 15:05:04 +0430

    initial comment

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import { movies } from "./api.js";
 
-console.log(movies);
+// select movie container which includes image and details.
+const moviesContent = document.querySelector(".flex-movies-container");
+// select movie details.
+const movieDetails = document.querySelector(".flex-card-box-details");
+
+//create image element
+const image = document.createElement("img");
+image.setAttribute("src", "./assets/img/bullet-train-movie.jpg");
+image.setAttribute("alt", "poster");
+image.className = "img-card";
+
+// create quality div tag
+const divQulity = document.createElement("div");
+divQulity.innerHTML = "HD";
+divQulity.className = "quality";
+
+// create movie detail content
+
+moviesContent.appendChild(image);
+moviesContent.appendChild(divQulity);
